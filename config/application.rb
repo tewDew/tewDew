50a187083709843a09a9eb2a79f80cbb0eec3e5c
joshneipp <joshneipp@gmail.com>
2015-10-18 17:57:20 -0400

    Add button style.

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,5 @@ module TewDew
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.precompile += ['jquery.js', 'jquery_ujs.js']
   end
 end
